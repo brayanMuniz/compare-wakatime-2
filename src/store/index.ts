6a47ed78c2c3c2cb4ea714226cf8f5027a16dc95
Erik E Sanchez <eriksanchez7247@gmail.com>
2020-06-30 10:53:31 -0500

    Got rid of unused store module

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,6 @@
 import Vue from "vue";
 import Vuex from "vuex";
 import userModule from "./userModule";
-import userData from './userData'
 Vue.use(Vuex);
 
 export default new Vuex.Store({
@@ -10,6 +9,5 @@ export default new Vuex.Store({
   actions: {},
   modules: {
     userModule,
-    userData
   },
 });

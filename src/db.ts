c4aac055c982a8b73b92e943de8f095e2fd85dde
Erik E Sanchez <eriksanchez7247@gmail.com>
2020-06-30 14:27:39 -0500

    Using wakatime-data instead of wakatime-classic fb

--- src/db.ts
+++ src/db.ts
@@ -1,12 +1,12 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC2ilXBezBS-Mx_UlrE_MczlluIlN11l08",
-  authDomain: "wakatimesaveddata.firebaseapp.com",
-  databaseURL: "https://wakatimesaveddata.firebaseio.com",
-  projectId: "wakatimesaveddata",
-  storageBucket: "wakatimesaveddata.appspot.com",
-  messagingSenderId: "375670754245",
-  appId: "1:375670754245:web:177a63d745fc4fab5a59ac",
+  apiKey: "AIzaSyA3RsY5YxNPsEogNCUDn2uGgy4-68AbRAY",
+  authDomain: "wakatime-data.firebaseapp.com",
+  databaseURL: "https://wakatime-data.firebaseio.com",
+  projectId: "wakatime-data",
+  storageBucket: "wakatime-data.appspot.com",
+  messagingSenderId: "863082391279",
+  appId: "1:863082391279:web:ebfdce1accf2ee6ed95d1a",
 };
 export const firebaseApp = firebase.initializeApp(firebaseConfig);

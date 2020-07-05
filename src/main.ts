7770b7ded7b2d863b747fbb0e7d7826e53ec7a77
Erik E Sanchez <eriksanchez7247@gmail.com>
2020-07-05 09:22:18 -0500

    Shows user data in order by dates

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,12 @@ import Router from "vue-router";
 import App from "@/App.vue";
 import router from "@/router";
 import store from "@/store";
+import moment from "moment"
 import "bootstrap";
 Vue.use(Router);
 Vue.config.productionTip = true;
+Vue.prototype.moment = moment
+
 
 new Vue({
   router,

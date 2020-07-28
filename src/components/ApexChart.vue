ec90428d830a0703523ad99275002bb7361600ba
Erik E Sanchez <eriksanchez7247@gmail.com>
2020-07-27 21:44:25 -0500

    npm install vue-apexcharts

--- src/components/ApexChart.vue
+++ src/components/ApexChart.vue
@@ -8,8 +8,26 @@ import ApexCharts from "apexcharts";
 
 export default Vue.extend({
   data() {
-    return {};
+    return {
+      options: {
+        chart: {
+          type: "line",
+        },
+        series: [
+          {
+            name: "sales",
+            data: [30, 40, 35, 50, 49, 60, 70, 91, 125],
+          },
+        ],
+        xaxis: {
+          categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999],
+        },
+      },
+    };
   },
+  mounted(){
+      let chart = new ApexCharts(do)
+  }
 });
 </script>
 

6f2f807bc83bccd1cd45a057d0c817fe371e4405
Erik E Sanchez <eriksanchez7247@gmail.com>
2020-07-27 21:41:36 -0500

    created apex charts file

---
+++ src/components/ApexChart.vue
@@ -0,0 +1,16 @@
+<template>
+  <div></div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import ApexCharts from "apexcharts";
+
+export default Vue.extend({
+  data() {
+    return {};
+  },
+});
+</script>
+
+<style></style>
